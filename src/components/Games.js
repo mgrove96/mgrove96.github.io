a7de636cb0622381e9b3943af83e0b13e1861e0b
Mitch Grove <mgrove96@gmail.com>
2017-09-13 01:20:47 -0700

    Implement hot cold and finish production build

--- src/components/Games.js
+++ src/components/Games.js
@@ -9,13 +9,22 @@ export default class Games extends React.Component {
 
     this.state = {
       hot: true,
-      isModalOpen: false
+      isModalOpen: false,
+      input: "",
+      secret: 101,
+      guessed: [],
+      feedback: ""
     }
   }
 
-  temperatureChange() {
+  componentWillMount() {
+    let randomNumber = Math.floor((Math.random() * 100) + 1);
+    this.setState({secret: randomNumber});
+  }
+
+  setTemperature(isHot) {
     const temp = this.state.hot;
-    this.setState({hot: !temp});
+    if (temp !== isHot) this.setState({hot: isHot});
   }
 
   openHowToPlay() {
@@ -26,8 +35,49 @@ export default class Games extends React.Component {
     this.setState({ isModalOpen: false })
   }
 
+  addGuess(userInput) {
+    let guessed = this.state.guessed.slice();
+    guessed.push(userInput);
+    this.setState({guessed: guessed});
+  }
+
+  getFeedback(userInput) {
+    this.addGuess(userInput);
+    const secret = this.state.secret;
+    if (userInput == secret) {
+      this.setState({feedback: "you got it!"});
+      this.setTemperature(true);
+    } else if (Math.abs(secret - userInput) < 10) {
+      this.setState({feedback: "hot!"});
+      this.setTemperature(true);
+    } else {
+      this.setState({feedback: "cold..."});
+      this.setTemperature(false);
+    }
+  }
+
+  handleInput(e) {
+    const inputVal = e.target.value;
+    this.setState({input: inputVal});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const userInput = this.state.input;
+    this.getFeedback(userInput); 
+  }
+
   render() {
+    let guessed = [];
+    const guessedNumbers = this.state.guessed;
+    let anyGuesses = false;
+    if (guessedNumbers.length > 0) anyGuesses = true;
+    for (let guess of guessedNumbers) {
+      let eachGuess = <div className="guess">{guess}</div>
+      guessed.push(eachGuess);
+    }
     const isHot = this.state.hot;
+    const feedback = this.state.feedback;
     return (
       <div>
       <span id="howToPlayLink" onClick={() => this.openHowToPlay()}>huh?</span>
@@ -44,11 +94,22 @@ export default class Games extends React.Component {
           <span id="gamesToHomeLink" onClick={() => { history.push('/') }}>home</span>
         )} />
         <div className={isHot ? "gamesView hot" : "gamesView cold"}>
-          <h1 onClick={() => this.temperatureChange()} className="gamesTitle">hot or cold?</h1>
+          <h1 className="gamesTitle">hot or cold?</h1>
           <div className="gameArea">
             <div className="inputArea">
+              <h1 className="inputTitle">please enter a number:</h1>
+              <div>
+                <form onSubmit={e => this.handleSubmit(e)}>
+                  <input type="number" id="inputVal" min="1" max="100" onChange={e => this.handleInput(e)} />
+                </form>
+              </div>
+              <h2 className={anyGuesses ? "guessedTitle" : "guessedTitle hidden"}>previous guesses:</h2>
+              <div className="guessedNumbers">
+                {guessed}
+              </div>
             </div>
             <div className="feedbackArea">
+              <h1 className="feedbackTitle">{feedback}</h1>
             </div>
           </div>
         </div>
